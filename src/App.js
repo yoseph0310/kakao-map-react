974012b2d4a7e947b50c61c50f1332b3c45a37c4
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-27 21:35:32 +0900

    t

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Location from "./Location"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      
+      <body>
+        <div id="map">
+          <Location />
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/Location.js
@@ -0,0 +1,32 @@
+/*global kakao*/ 
+import React, { useEffect } from 'react'
+
+
+
+const Location=()=>{
+
+  useEffect(()=>{
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      level: 3
+    };
+
+    var map = new kakao.maps.Map(container, options);
+    var markerPosition  = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488); 
+    var marker = new kakao.maps.Marker({
+      position: markerPosition
+  });
+  marker.setMap(map);
+
+    }, [])
+
+
+    return (
+        
+      <div id="map" style={{width:"100%", height:"500px"}}></div>
+        
+    )
+}
+
+export default Location;

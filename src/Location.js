e08b9fbbad22d3ba37388c61ffdc6ad627b2244a
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-09-28 20:44:55 +0900

    :recycle: 크기 수정

--- src/Location.js
+++ src/Location.js
@@ -51,7 +51,7 @@ const Location = () => {
 
   return (
 
-    <div id="map" style={{ width: "100%", height: "100%" }}></div>
+    <div id="map" style={{ width: "100%", height: "800px" }}></div>
 
   )
 }
